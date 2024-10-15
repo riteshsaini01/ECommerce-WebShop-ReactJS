4e1fd3c8a8517e9b520b46dcb2588e990baf1bed
riteshsaini01 <b210885@skit.ac.in>
2024-10-16 03:38:08 +0530

    Added home page

--- Frontend/src/component/Navbar.jsx
+++ Frontend/src/component/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { assets } from '../assets/assets';
 import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const[visible,setVisible]= useState(false);
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       {/* Logo */}
@@ -12,11 +15,11 @@ const Navbar = () => {
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
         <NavLink 
           to='/' 
-          className='flex flex-col items-center gap-1 hover:text-black'
+          className='flex flex-col items-center gap-1 hover:text-black '
           activeClassName='text-black'
         >
           <p>HOME</p>
-          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hover:block' />
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hover:block hidden' />
         </NavLink>
 
         <NavLink 
@@ -25,7 +28,7 @@ const Navbar = () => {
           activeClassName='text-black'
         >
           <p>COLLECTION</p>
-          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hover:block' />
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hover:block hidden' />
         </NavLink>
 
         <NavLink 
@@ -34,7 +37,7 @@ const Navbar = () => {
           activeClassName='text-black'
         >
           <p>ABOUT</p>
-          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hover:block' />
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hover:block hidden' />
         </NavLink>
 
         <NavLink 
@@ -43,7 +46,7 @@ const Navbar = () => {
           activeClassName='text-black'
         >
           <p>CONTACT</p>
-          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hover:block' />
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hover:block hidden' />
         </NavLink>
       </ul>
 
@@ -67,8 +70,27 @@ const Navbar = () => {
         {/* Cart Icon */}
         <Link to='/cart' className='relative'>
           <img src={assets.cart_icon} className='w-5 min-w-5' alt='Cart' />
-          <p className=''></p>
+          <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
         </Link>
+
+        <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt=''/>
+      </div>
+
+      {/* sidebar menu for small screens */}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+        <div className='flex flex-col text-gray-600'>
+          <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer' >
+            <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+            <p>Back</p>
+          </div>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+        </div>
+
+
+
       </div>
     </div>
   );
