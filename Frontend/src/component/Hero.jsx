9d98e5dba4c39e0fcccecb5745c9f206ca2e86b0
riteshsaini01 <b210885@skit.ac.in>
2024-10-16 03:42:40 +0530

    resolved css mistakes

--- Frontend/src/component/Hero.jsx
+++ Frontend/src/component/Hero.jsx
@@ -4,22 +4,22 @@ import { assets } from '../assets/assets'
 const Hero = () => {
   return (
     <div className='flex flex-col sm:flex-row border border-gray-400'>
-        {/* Hero left side */}
-        <div className='w-full sm:w-1/2 flex item-center justify-center py-10 sm:py-0'>
-            <div className='text-[#414141]'>
-                <div className='flex items-center gap-2'>
-                    <p className='w-8 md:w-11 h-[2] bg-[#414141]'></p>
-                    <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
-                </div>
-                <h1 className='text-3xl sm:py-3 lg:text-5x1 leading-relaxed'>LATEST ARRIVALS</h1>
-                <div className='flex items-center gap-2'>
-                    <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
-                    <p className='w-8 md:w-11 h-[2] bg-[#414141]'></p>               
-                </div>
-            </div>
+      {/* Hero left side */}
+      <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+        <div className='text-[#414141]'>
+          <div className='flex items-center gap-2'>
+            <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p> {/* Fixed height */}
+            <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
+          </div>
+          <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>LATEST ARRIVALS</h1> {/* Corrected xl class */}
+          <div className='flex items-center gap-2'>
+            <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
+            <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p> {/* Fixed height */}
+          </div>
         </div>
-        {/* Hero right side */}
-        <img className='w-full smw-1/2' src={assets.hero_img} alt="" />
+      </div>
+      {/* Hero right side */}
+      <img className='w-full sm:w-1/2' src={assets.hero_img} alt="Hero Image" /> {/* Fixed typo sm:w-1/2 */}
     </div>
   )
 }
