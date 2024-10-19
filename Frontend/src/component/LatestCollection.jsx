e66028da4d11d05f35b540247f5382342edd6e64
riteshsaini01 <b210885@skit.ac.in>
2024-10-19 21:29:52 +0530

    Added latest collections

--- Frontend/src/component/LatestCollection.jsx
+++ Frontend/src/component/LatestCollection.jsx
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
 
 const LatestCollection = () => {
 
     const { products } = useContext(ShopContext);
 
-    console.log(products);
+    
 
   return (
-    <div>LatestCollection</div>
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTIONS'} /> 
+        <p className='-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit aperiam vel quam nam illum obcaecati facere, nesciunt doloremque repellat dignissimos a mollitia at maxime velit officia debitis blanditiis ea excepturi.
+        </p>
+      </div>
+    </div>
   )
 }
 
