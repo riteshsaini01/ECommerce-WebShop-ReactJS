4e1ed943183d5375c288c8f2406d4fb17129af44
riteshsaini01 <b210885@skit.ac.in>
2024-10-21 17:44:22 +0530

    Added functionalities

--- Frontend/src/component/LatestCollection.jsx
+++ Frontend/src/component/LatestCollection.jsx
@@ -13,7 +13,7 @@ const LatestCollection = () => {
       <div className='text-center py-8 text-3xl'>
         <Title text1={'LATEST'} text2={'COLLECTIONS'} /> 
         <p className='-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit aperiam vel quam nam illum obcaecati facere, nesciunt doloremque repellat dignissimos a mollitia at maxime velit officia debitis blanditiis ea excepturi.
+          Lorem ipsum, dolor sit amet 
         </p>
       </div>
     </div>
