13d02379cc7fdfc147e0b511f5e0218d6a64e0c3
riteshsaini01 <b210885@skit.ac.in>
2024-10-22 19:33:04 +0530

    Added latest collection products

--- Frontend/src/component/Navbar.jsx
+++ Frontend/src/component/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       {/* Logo */}
-      <img src={assets.logo} className='w-36' alt='Logo' />
+    <Link to='/'>  <img src={assets.logo} className='w-36' alt='Logo' /></Link>
 
       {/* Navigation Links */}
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
