97bab31356e29492d6d649d17c6f305f8de60197
riteshsaini01 <b210885@skit.ac.in>
2024-10-26 14:43:24 +0530

    Added functionalities in footer

--- Frontend/src/component/Footer.jsx
+++ Frontend/src/component/Footer.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+          <img src={assets.logo} className='' alt="" />
+        </div>
+
+      </div>
     </div>
   )
 }
