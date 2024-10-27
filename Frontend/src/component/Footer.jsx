17c70996423f19d391aa4dc0ab8fbd28bbb41863
riteshsaini01 <b210885@skit.ac.in>
2024-10-27 18:26:04 +0530

    Added functionalities

--- Frontend/src/component/Footer.jsx
+++ Frontend/src/component/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
         </div>
 
         <div>
-          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <p cla ssName='text-xl font-medium mb-5'>COMPANY</p>
           <ul className='flex flex-col gap-1 text-gray-600'>
             <li>Home</li>
             <li>About us</li> 
