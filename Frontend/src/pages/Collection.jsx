b528cd507efb26de07894752d2f6336bfa74b4c1
riteshsaini01 <b210885@skit.ac.in>
2024-10-30 19:10:09 +0530

    update

--- Frontend/src/pages/Collection.jsx
+++ Frontend/src/pages/Collection.jsx
@@ -27,6 +27,20 @@ const Collection = () => {
           </div>
         </div>
         {/* Subcategory Filter */}
+        <div className="mt-4 border border-gray-200 rounded-lg shadow-sm p-4 sm:block bg-white">
+          <p className="mb-3 text-sm font-semibold text-gray-800">CATEGORIES</p>
+          <div className="flex flex-col gap-4 text-sm font-light text-gray-700">
+            <p className="flex items-center gap-2">
+              <input className="w-4 h-4 rounded border-gray-300" type="checkbox" value="Men" /> Men
+            </p>
+            <p className="flex items-center gap-2">
+              <input className="w-4 h-4 rounded border-gray-300" type="checkbox" value="Women" /> Women
+            </p>
+            <p className="flex items-center gap-2">
+              <input className="w-4 h-4 rounded border-gray-300" type="checkbox" value="Kids" /> Kids
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
